53b107e7d34aee31eb98acd9785d15590b205f1c
DeftDev <DeftDev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-21 14:06:01 +0000

    removing this incorrectly placed folder.
    git-svn-id: https://plugins.svn.wordpress.org/resres-restaurant-reservations/trunk@952209 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 1.0.2.f/public/views/cancel.php
+++
@@ -1,9 +0,0 @@
-<form id="resres_cancel_form" action="" method="POST">
-
-<label for="resres_cancel">Enter your reservation ID to cancel.</label>
-<input type="text" id="resres_cancel" name="resres_cancel" value="" />
-<?php wp_nonce_field( 'resres_cancel' ); ?>
-<input type="submit" id="resres_cancel_submit" name="resres_cancel_submit" value="<?php echo __('Cancel Reservation'); ?>"  />
-
-
-</form>
